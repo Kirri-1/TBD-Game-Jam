61d02758628e9b5ab0576e0184303ba26e333bf2
Kirri-1 <kirridesu@gmail.com>
2025-09-12 17:52:45 -0500

    updated movement

--- Assets/Scripts/Player Scripts/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 
@@ -9,12 +10,14 @@ public class PlayerMovement : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField]
-    float m_moveSpeed = 5f;
+    private bool m_isMoving;
+    private Vector3 origPos, targetPos;
+    private float m_timeToMove = 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         m_playerRb = GetComponent<Rigidbody2D>();
-        DebugHelper.CriticalNullReferenceChecker(this, typeof(Rigidbody2D), "Start()", "GetComponent<Rigidbody2D>()", gameObject, m_playerRb != null);
+        DebugHelper.CriticalNullReferenceLogger(this, typeof(Rigidbody2D), "Start()", "GetComponent<Rigidbody2D>()", gameObject, m_playerRb != null);
         //shouldn't activate due to RequireComponent attribute, but just in case
         m_playerRb.gravityScale = 0f;
     }
@@ -26,11 +29,67 @@ public class PlayerMovement : MonoBehaviour
 
     void Movement()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        if (!m_isMoving)
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
 
-        Vector2 movement = new Vector2(horizontal, vertical).normalized * m_moveSpeed;
+            if (horizontal < 0)
+            {
+                StartCoroutine(MovePlayer(Vector3.left));
+            }
+            else if(horizontal > 0)
+            {
+                StartCoroutine(MovePlayer(Vector3.right));
+            }
 
-        m_playerRb.linearVelocity = new Vector2(movement.x, movement.y);
+            if (vertical < 0)
+            {
+                StartCoroutine(MovePlayer(Vector3.down));
+            }
+            else if (vertical > 0)
+            {
+                StartCoroutine(MovePlayer(Vector3.up));
+            }
+
+
+
+            /*if (Input.GetKey(KeyCode.W))
+            {
+                StartCoroutine(MovePlayer(Vector3.up));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                StartCoroutine(MovePlayer(Vector3.down));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                StartCoroutine(MovePlayer(Vector3.left));
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                StartCoroutine(MovePlayer(Vector3.right));
+            }*/
+        }
+    }
+    private IEnumerator MovePlayer(Vector3 direction)
+    {
+        m_isMoving = true;
+
+        float elapsedTime = 0;
+
+        origPos = transform.position;
+        targetPos = origPos + direction;
+
+        while (elapsedTime < m_timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / m_timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+
+        m_isMoving = false;
     }
 }
