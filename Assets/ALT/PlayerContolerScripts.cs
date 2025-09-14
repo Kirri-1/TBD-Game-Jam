0d70eb30ccdd559344970e026f425841261ab788
Thair <Tahir2.Serali@live.uwe.ac.uk>
2025-09-14 20:14:18 +0100

    Grid based Movement

--- Assets/ALT/PlayerContolerScripts.cs
+++ Assets/ALT/PlayerContolerScripts.cs
@@ -2,18 +2,46 @@ using UnityEngine;
 
 public class PlayerContolerScripts : MonoBehaviour
 {
+    [SerializeField]
+    private bool isMoving = false;
+
     public float moveSpeed = 5;
     public Transform movePoint;
+    public float gridOffset = 0.5f;
+    public LayerMask colidables;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         movePoint.parent = null;
+        movePoint.position = this.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // get input
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+
+        // Move the postion of the player
+        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+
+        // If player has moved enough away from the start to end point 
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
+        {
+            if (Mathf.Abs(xInput) == 1f)
+            {
+                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(xInput, 0f, 0f), .2f, colidables))
+                    movePoint.position += new Vector3(xInput, 0f, 0f);
+            }
+
+            if (Mathf.Abs(yInput) == 1f)
+            {
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, yInput, 0f), .2f, colidables))
+
+                    movePoint.position += new Vector3(0f, yInput, 0f);
+            }
+        }
     }
 }
