b3149c9f3c7bf655c4a7ff598655c9431a795f1b
Thair <Tahir2.Serali@live.uwe.ac.uk>
2025-09-15 02:36:07 +0100

    Got the movement and power cell movable

--- Assets/ALT/PlayerContolerScripts.cs
+++ Assets/ALT/PlayerContolerScripts.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class PlayerContolerScripts : MonoBehaviour
 {
-    [SerializeField]
-    private bool isMoving = false;
+    public bool isMoving = false;
+    public bool isHoldingCell = false;
 
     public float moveSpeed = 5;
     public Transform movePoint;
     public float gridOffset = 0.5f;
-    public LayerMask colidables;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Space(10)]
+    public LayerMask colidables;
+    public LayerMask pickableCell;
+
+    [Space(10)]
+    public MovingDir MovingDirEnms = MovingDir.up;
+    public KeyCode pickUpPowerCellKey = KeyCode.Space;
+
     void Start()
     {
         movePoint.parent = null;
@@ -24,24 +30,57 @@ public class PlayerContolerScripts : MonoBehaviour
         float xInput = Input.GetAxisRaw("Horizontal");
         float yInput = Input.GetAxisRaw("Vertical");
 
+        // X dir
+        if (xInput <= -1)
+            MovingDirEnms = MovingDir.left;
+
+        else if (xInput >= 1)
+            MovingDirEnms = MovingDir.right;
+
+        // Y dir
+        else if (yInput <= -1)
+            MovingDirEnms = MovingDir.down;
+
+        else if (yInput >= 1)
+            MovingDirEnms = MovingDir.up;
+
+
+        // Add extra space for power cell
+        int addCellWith = 0;
+        if (isHoldingCell)
+            addCellWith = 1;
+
         // Move the postion of the player
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
         // If player has moved enough away from the start to end point 
         if (Vector3.Distance(transform.position, movePoint.position) <= 0.05f)
         {
+
             if (Mathf.Abs(xInput) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(xInput, 0f, 0f), .2f, colidables))
+                if (xInput < 0)
+                    addCellWith = -addCellWith;
+
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(xInput + addCellWith, 0f, 0f), .2f, colidables))
                     movePoint.position += new Vector3(xInput, 0f, 0f);
             }
 
             if (Mathf.Abs(yInput) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, yInput, 0f), .2f, colidables))
+                if (yInput < 0)
+                    addCellWith = -addCellWith;
 
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, yInput + addCellWith, 0f), .2f, colidables))
                     movePoint.position += new Vector3(0f, yInput, 0f);
             }
         }
+
+
+        if (Input.GetKey(pickUpPowerCellKey))
+            isHoldingCell = true;
+
+        else if (Input.GetKeyUp(pickUpPowerCellKey))
+            isHoldingCell = false;
     }
 }
